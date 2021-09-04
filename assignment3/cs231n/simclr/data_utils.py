c72872bd711cf4909df9492780b1e7653c77fdc8
钱旭 <1464238196@qq.com>
2021-09-05 01:38:25 +0800

    add self supervised learning impl

--- assignment3/cs231n/simclr/data_utils.py
+++ assignment3/cs231n/simclr/data_utils.py
@@ -4,6 +4,7 @@ from torchvision.datasets import CIFAR10
 import random
 import torch
 
+
 def compute_train_transform(seed=123456):
     """
     This function returns a composition of data augmentations to a single training image.
@@ -11,10 +12,10 @@ def compute_train_transform(seed=123456):
     """
     random.seed(seed)
     torch.random.manual_seed(seed)
-    
+
     # Transformation that applies color jitter with brightness=0.4, contrast=0.4, saturation=0.4, and hue=0.1
-    color_jitter = transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)  
-    
+    color_jitter = transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)
+
     train_transform = transforms.Compose([
         ##############################################################################
         # TODO: Start of your code.                                                  #
@@ -25,15 +26,19 @@ def compute_train_transform(seed=123456):
         # Step 1: Randomly resize and crop to 32x32.
         transforms.RandomResizedCrop(32),
         # Step 2: Horizontally flip the image with probability 0.5
+        transforms.RandomHorizontalFlip(p=0.5),
         # Step 3: With a probability of 0.8, apply color jitter (you can use "color_jitter" defined above.
+        transforms.RandomApply([color_jitter], p=0.8),
         # Step 4: With a probability of 0.2, convert the image to grayscale
+        transforms.RandomGrayscale(p=0.2),
         ##############################################################################
         #                               END OF YOUR CODE                             #
         ##############################################################################
         transforms.ToTensor(),
         transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
     return train_transform
-    
+
+
 def compute_test_transform():
     test_transform = transforms.Compose([
         transforms.ToTensor(),
@@ -44,6 +49,7 @@ def compute_test_transform():
 class CIFAR10Pair(CIFAR10):
     """CIFAR10 Dataset.
     """
+
     def __getitem__(self, index):
         img, target = self.data[index], self.targets[index]
         img = Image.fromarray(img)
@@ -57,6 +63,8 @@ class CIFAR10Pair(CIFAR10):
             #                                                                            #
             # Apply self.transform to the image to produce x_i and x_j in the paper #
             ##############################################################################
+            x_i = self.transform(img)
+            x_j = self.transform(img)
             ##############################################################################
             #                               END OF YOUR CODE                             #
             ##############################################################################
